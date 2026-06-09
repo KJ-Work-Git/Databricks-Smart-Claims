fb97d5f27e89fb0e9ebe97516fe8be7911e2861f
Kieran Pritchard <kj_worker@proton.me>
2026-06-09 12:59:01 +0100

    Update 01 - bronze.py

--- 01 - Data Integration/Transformations/01 - bronze.py
+++ 01 - Data Integration/Transformations/01 - bronze.py
@@ -30,7 +30,7 @@ spark.readStream.format("cloudFiles")
 # Contains policy information: policy numbers, dates, premiums, location details
 # ----------------------------------
 @dp.table(comment="The raw policy data loaded from csv files.")
-def raw_claim():
+def raw_policy():
 return (
 spark.readStream.format("cloudFiles")
 .option("cloudFiles.format", "csv")
@@ -42,9 +42,10 @@ spark.readStream.format("cloudFiles")
 # Contains vehicle telemetry: speed, GPS coordinates, chassis numbers
 # ----------------------------------
 @dp.table(comment="The raw telematics data loaded from csv files.")
-def raw_claim():
+def raw_telematics():
 return (
 spark.readStream.format("cloudFiles")
 .option("cloudFiles.format", "csv")
 .option("cloudFiles.inferColumnTypes", "true")
 .load(f"/Volumes/{catalog}/{db}/{volume_name}/DSBC_Telematics.csv"))
+  
